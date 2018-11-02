f7a5cfa754ec49d0a8daf9532a06bc81e2b5575a
Kartoophilus <30388052+kartoophilus@users.noreply.github.com>
2018-11-02 09:38:46 +0200

    Create getRandomColor.js
    A random color generator in js

---
+++ getRandomColor.js
@@ -0,0 +1,10 @@
+//This function generates random colors
+function getRandomColor(){
+  var letters = "0123456789ABCDEF";
+  var color = '#';
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random()*16)];
+  }
+  return color
+}
+//https://stackoverflow.com/questions/1484506/random-color-generator#1484514
